fa9db70054401b1bd3ed7f99b5d955671cdfb04d
Jacek Kopaczel <jacek.kopaczel@swifty.global>
2022-01-20 14:29:18 +0100

    Initial Commit

---
+++ HDWalletDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HDWalletDemo
+//
+//  Created by Jacek Kopaczel on 20/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
