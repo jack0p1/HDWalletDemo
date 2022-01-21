9220c5ce2290dfdb73c69873731860a849e878f8
Jacek Kopaczel <jacek.kopaczel@swifty.global>
2022-01-21 15:38:27 +0100

    Minor refactor

---
+++ HDWalletDemo/Scenes/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  HDWalletDemo
+//
+//  Created by Jacek Kopaczel on 20/01/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func createWalletButtonTouched(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func importWalletButtonTouched(_ sender: UIButton) {
+        
+    }
+}
+
