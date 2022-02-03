e2a3f86c5f0a98e0a6cabe9ed5d038cacce9585e
Jacek Kopaczel <jacek.kopaczel@swifty.global>
2022-02-03 13:48:19 +0100

    Update BalanceViewModel.swift

--- HDWalletDemo/Scenes/Balance/BalanceViewModel.swift
+++ HDWalletDemo/Scenes/Balance/BalanceViewModel.swift
@@ -56,33 +56,6 @@ class BalanceViewModel {
                 self?.balanceLoaded.send()
             }
         }
-        
-//        let group = DispatchGroup()
-//        group.enter()
-//        DataManager.shared.getEthBalance(for: wallet.address) { [weak self] in
-//            self?.ethBalance.send("Ethereum (ETH): " + ($0 ?? "-"))
-//            group.leave()
-//        }
-//
-//        group.enter()
-//        DataManager.shared.getTokenBalance(for: wallet.address, token: TokenContract.chainLink) { [weak self] balance, name, symbol in
-//            if let balance = balance, let name = name, let symbol = symbol {
-//                self?.chainLinkBalance.send("\(name) (\(symbol)): \(balance)")
-//            }
-//            group.leave()
-//        }
-//
-//        group.enter()
-//        DataManager.shared.getTokenBalance(for: wallet.address, token: TokenContract.gibboToken) { [weak self] balance, name, symbol in
-//            if let balance = balance, let name = name, let symbol = symbol {
-//                self?.gibboTokenBalance.send("\(name) (\(symbol)): \(balance)")
-//            }
-//            group.leave()
-//        }
-//
-//        group.notify(queue: .main) { [weak self] in
-//            self?.balanceLoaded.send()
-//        }
     }
     
     func routeToSendBalance() {
