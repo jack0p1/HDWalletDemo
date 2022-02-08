20e7334f2fc0b4a9fdcfffa380591d8df8576185
Jacek Kopaczel <jacek.kopaczel@swifty.global>
2022-02-08 10:00:02 +0100

    Updated ,gitignore. Added Constants.swift file with placeholders

---
+++ HDWalletDemo/Supporting files/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  HDWalletDemo
+//
+//  Created by Jacek Kopaczel on 25/01/2022.
+//
+
+import Foundation
+
+enum Constants {
+    static let ropstenEndpoint = "YOUR ROPSTEN ENDPOINT"
+    static let rinkebyEndpoint = "YOUR RINKEBY ENDPOINT"
+}
