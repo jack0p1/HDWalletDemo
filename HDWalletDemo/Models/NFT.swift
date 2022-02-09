73ba3fe336ddb0ca23b4e53c8cd4a3fa33aa6354
Jacek Kopaczel <jacek.kopaczel@swifty.global>
2022-02-09 17:07:21 +0100

    NFT cell creation

--- HDWalletDemo/Models/NFT.swift
+++ HDWalletDemo/Models/NFT.swift
@@ -5,11 +5,13 @@
 //  Created by Jacek Kopaczel on 09/02/2022.
 //
 
-import Foundation
+import UIKit
 
 struct NFT {
     let contractAddress: String
     let tokenId: String
     let name: String
-    let image: URL
+    let imageUrl: URL
+    
+    var image: UIImage?
 }
--- HDWalletDemo/Scenes/NFTs/NFTsViewModel.swift
+++ HDWalletDemo/Scenes/NFTs/NFTsViewModel.swift
@@ -24,17 +24,34 @@ extension NFTsViewModel: UITableViewDelegate {
 
 extension NFTsViewModel: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        nfts.count
+        nfts.isEmpty ? 1 : nfts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "nftTableViewCell", for: indexPath)
+        
+        guard var nft = nfts[safe: indexPath.row] else {
+            return cell
+        }
+        
         var content = cell.defaultContentConfiguration()
-        content.text = nfts[safe: indexPath.row]?.name
-        content.image = 
+        content.text = nft.name
+        
+        if let image = nft.image {
+            content.image = image
+        } else {
+            DispatchQueue.global(qos: .utility).async {
+                if let data = try? Data(contentsOf: nft.imageUrl) {
+                    DispatchQueue.main.async {
+                        let nftImage = UIImage(data: data)
+                        content.image = nftImage
+                        nft.image = nftImage
+                    }
+                }
+            }
+        }
+        
         cell.contentConfiguration = content
         return cell
     }
-    
-    
 }
